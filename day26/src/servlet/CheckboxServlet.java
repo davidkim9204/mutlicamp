c360c9512d00e85e11411126d56bb558c714da0f
david <david@DESKTOP-S4BRSPA>
2017-09-26 09:03:04 +0900

    day26

--- day26/src/servlet/CheckboxServlet.java
+++ day26/src/servlet/CheckboxServlet.java
@@ -10,24 +10,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class LoginServlet
+ * Servlet implementation class CheckboxServlet
  */
-@WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet {
+@WebServlet("/CheckboxServlet")
+public class CheckboxServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
-		String id = request.getParameter("id");
-		String pwd = request.getParameter("pwd");
 		PrintWriter out = response.getWriter();
 		out.print("<html><body>");
-		out.println("당신이 입력한 정보입니다.<br>");
-		out.println("아이디 : ");
-		out.println(id);
-		out.println("<br> 비밀번호 : ");
-		out.println(pwd);
+		String items[] = request.getParameterValues("item");
+		if (items == null) {
+			out.print("선택한 항목이 없습니다.");
+		} else {
+			out.println("당신이 선택한 항목입니다.<hr>");
+			for (String item : items) {
+				out.print(item + " ");
+			}
+		}
 		out.println("<br><a href='javascript:history.go(-1)'>다시</a>");
 		out.print("</body></html>");
 		out.close();
--- day26/src/servlet/RadioServlet.java
+++ day26/src/servlet/RadioServlet.java
@@ -10,25 +10,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class LoginServlet
+ * Servlet implementation class RadioServlet
  */
-@WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet {
+@WebServlet("/RadioServlet")
+public class RadioServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
-		String id = request.getParameter("id");
-		String pwd = request.getParameter("pwd");
+		String gender = request.getParameter("gender");
+		String chk_mail = request.getParameter("chk_mail");
+		String content = request.getParameter("content");
 		PrintWriter out = response.getWriter();
 		out.print("<html><body>");
-		out.println("당신이 입력한 정보입니다.<br>");
-		out.println("아이디 : ");
-		out.println(id);
-		out.println("<br> 비밀번호 : ");
-		out.println(pwd);
-		out.println("<br><a href='javascript:history.go(-1)'>다시</a>");
+		out.println("당신이 입력한 정보입니다.<hr>");
+		out.println("성별 : <b>");
+		out.println(gender);
+		out.println("</b><br> 메일 정보 수신 여부 : <b>");
+		out.println(chk_mail);
+		out.println("</b><br> 가입 인사 : <b><pre>");
+		out.println(content);
+		out.println("</b></pre><a href='javascript:history.go(-1)'>다시</a>");
 		out.print("</body></html>");
 		out.close();
 	}
